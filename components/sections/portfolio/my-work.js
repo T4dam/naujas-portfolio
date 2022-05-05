ee2044849f0036591b53f8c714402432a10e009f
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-05 22:12:13 +0300

    portfolio page

--- components/sections/portfolio/my-work.js
+++ components/sections/portfolio/my-work.js
@@ -6,25 +6,26 @@ const Mywork = () => {
     {
       title: 'Tasky',
       intro: 'A handy dandy time management tool for planners and time savers. You can easily track your task process with a drag and drop functionality',
-      src: '/'
+      src: '/images/tasky.jpg'
     },
     {
-      title: 'Server Page',
+      title: 'GameDungeon',
       intro: 'A website for a funky looking fictional company that offers variety of services for gamers and beyond. ',
-      src: '/'
+      src: '/images/dungeon.jpg'
     },
     {
-      title: 'Song cover',
+      title: 'Song cover image',
       intro: 'Collaborated with VeeHouse and famous Lithuanian hip-hop band "Dogma" for a song cover for youtube of their latest single called "Karalius nuogas".',
-      src: '/'
+      src: '/images/dogma.jpeg',
+      alt: 'Song cover photo'
     },
   ]
   return (
     <section className='h-full w-full bg-slate-900 text-white flex justify-center items-center'>
-    <div className='container  snap-proximity max-w-6xl mt-32 w-full  p-4 mx-auto'>
+    <div className='container snap-y snap-mandatory overflow-scroll max-w-6xl mt-32 w-full  p-4 mx-auto'>
         {data.map((item, i)=> {
             return (
-        <div key={i} className='snap-start flex-col '>
+        <div key={i} className='flex-col '>
             {/* <div className='flex justify-center mb-12 text-4xl font-bold'>About</div> */}
                        <div className=' w-full h-auto text-center md:text-center flex flex-col sm:flex-row gap-12 justify-between'>
                     <div className='flex justify-center sm:justify-start items-center snap-center'>
@@ -39,9 +40,11 @@ const Mywork = () => {
                                    </div>
                     </div>
                     </div>
-                       <div className='w-full flex justify-center sm:justify-end items-center'>
+                       <div className='snap-start w-full flex justify-center sm:justify-end items-center'>
                        {/* <Image src="https://uploads-ssl.webflow.com/5ff9747066ed90b013f60409/615288994a29b449db5e33ec_zerohaul-duct-tape-thumbnail.jpg" layout='fill' className='w-[85vw] md:w-[42vw] lg:w-[38vw] xl:w-[40vw]' style="transform: translate3d(0vw, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d; opacity: 1;"  srcset="https://uploads-ssl.webflow.com/5ff9747066ed90b013f60409/615288994a29b449db5e33ec_zerohaul-duct-tape-thumbnail-p-800.jpeg 800w, https://uploads-ssl.webflow.com/5ff9747066ed90b013f60409/615288994a29b449db5e33ec_zerohaul-duct-tape-thumbnail.jpg 1080w" alt="Zerohaul Logistics Logo on Business Cards Floating" class="project-image" /> */}
-                        <div className=' bg-green-300 h-[250px] w-[250px] sm:h-[340px] sm:w-[340px] md:h-[420px] md:w-[420px] lg:h-[550px] lg:w-[550px] rounded-3xl max-w-4xl max-h-4xl'></div>
+                        <div className=' bg-green-300 h-[250px] w-[250px] sm:h-[340px] sm:w-[340px] md:h-[420px] md:w-[420px] lg:h-[550px] lg:w-[550px] rounded-3xl max-w-4xl max-h-4xl relative overflow-hidden'>
+                          <Image src={item.src} alt={item.alt} layout='fill' objectFit='cover' />
+                        </div>
                        </div>
                        </div>
                        <div className='h-1 md:h-1 w-full bg-green-400 my-20'></div>
