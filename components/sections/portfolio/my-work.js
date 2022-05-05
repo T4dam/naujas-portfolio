31ce9a36b6d9a479a61dc6f5bca2ab197cdc1fac
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-05 15:34:35 +0300

    portfolio page layout

--- components/sections/portfolio/my-work.js
+++ components/sections/portfolio/my-work.js
@@ -2,27 +2,53 @@ import React from 'react'
 import Image from 'next/image'
 
 const Mywork = () => {
+  const data = [
+    {
+      title: 'Tasky',
+      intro: 'A handy dandy time management tool for planners and time savers. You can easily track your task process with a drag and drop functionality',
+      src: '/'
+    },
+    {
+      title: 'Server Page',
+      intro: 'A website for a funky looking fictional company that offers variety of services for gamers and beyond. ',
+      src: '/'
+    },
+    {
+      title: 'Song cover',
+      intro: 'Collaborated with VeeHouse and famous Lithuanian hip-hop band "Dogma" for a song cover for youtube of their latest single called "Karalius nuogas".',
+      src: '/'
+    },
+  ]
   return (
     <section className='h-full w-full bg-slate-900 text-white flex justify-center items-center'>
-    <div className='container  mt-32 w-full  mx-auto flex justify-center items-center'>
-        <div className='flex-col mb-32'>
-            <div className='flex justify-center mb-12 text-4xl font-bold'>About</div>
-                       <div className=' w-full max-w-6xl h-auto text-center md:text-center flex flex-col md:flex-row gap-12 justify-between'>
-                    <div className='w-full md:w-1/2'>
-                        <h3 className='text-3xl md:text-3xl font-bold md:text-left p-4'>Quality is remembered  <br className='' /> long after price is forgotten</h3>
-                
-             <div className='text-left'>
-                 <button className='shadow-md bg-green-400 p-2 w-44 md:w-72 rounded-[4px] hover:bg-green-300'>
-                      <p>Say Hello</p>
-                    </button>
-             </div>
+    <div className='container  snap-proximity max-w-6xl mt-32 w-full  p-4 mx-auto'>
+        {data.map((item, i)=> {
+            return (
+        <div key={i} className='snap-start flex-col '>
+            {/* <div className='flex justify-center mb-12 text-4xl font-bold'>About</div> */}
+                       <div className=' w-full h-auto text-center md:text-center flex flex-col sm:flex-row gap-12 justify-between'>
+                    <div className='flex justify-center sm:justify-start items-center snap-center'>
+                      <div className='max-w-[84%]'>
+                          <h3 className='text-4xl md:text-6xl font-black text-center sm:text-left mb-6'>{item.title}</h3>
+                          <p className='text-center sm:text-left sm:text-lg leading-7 mb-6'>{item.intro}</p>
+                                      
+                                   <div className='text-center sm:text-left '>
+                                       <button className='shadow-md bg-green-400 p-2 w-44 md:w-72 rounded-[4px] hover:bg-green-300'>
+                        <p>Learn more</p>
+                      </button>
+                                   </div>
                     </div>
-                       <div className=' w-full p-4 md:w-1/2'>
+                    </div>
+                       <div className='w-full flex justify-center sm:justify-end items-center'>
                        {/* <Image src="https://uploads-ssl.webflow.com/5ff9747066ed90b013f60409/615288994a29b449db5e33ec_zerohaul-duct-tape-thumbnail.jpg" layout='fill' className='w-[85vw] md:w-[42vw] lg:w-[38vw] xl:w-[40vw]' style="transform: translate3d(0vw, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg); transform-style: preserve-3d; opacity: 1;"  srcset="https://uploads-ssl.webflow.com/5ff9747066ed90b013f60409/615288994a29b449db5e33ec_zerohaul-duct-tape-thumbnail-p-800.jpeg 800w, https://uploads-ssl.webflow.com/5ff9747066ed90b013f60409/615288994a29b449db5e33ec_zerohaul-duct-tape-thumbnail.jpg 1080w" alt="Zerohaul Logistics Logo on Business Cards Floating" class="project-image" /> */}
-                        <div className='bg-green-300 h-[500px] w-[500px] rounded-3xl max-w-4xl max-h-4xl'></div>
+                        <div className=' bg-green-300 h-[250px] w-[250px] sm:h-[340px] sm:w-[340px] md:h-[420px] md:w-[420px] lg:h-[550px] lg:w-[550px] rounded-3xl max-w-4xl max-h-4xl'></div>
                        </div>
                        </div>
+                       <div className='h-1 md:h-1 w-full bg-green-400 my-20'></div>
         </div>
+            )
+
+        })}
     </div>
 </section>
   )
