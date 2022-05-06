680ccfed5a302ea421adb4fd069f488c769ce1e9
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-06 21:06:01 +0300

    fixes

--- pages/contact.js
+++ pages/contact.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Layout from '../components/layouts';
-import Form from './../components/Form/from';
+// import Form from './../components/Form/from';
 
 
 const Contact = () => {
   return (
  <Layout>
-        <Form />
+        {/* <Form /> */}
  </Layout>
   )
 }
