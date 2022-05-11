d21454494a4cc517945d2b2ae82bc23b9408aad7
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-11 09:44:17 +0300

    title gradients

--- components/sections/portfolio/portfolio-hero.js
+++ components/sections/portfolio/portfolio-hero.js
@@ -6,7 +6,7 @@ const PorfolioHero = () => {
       <section className='bg-slate-900 h-screen -mt-14'>
         <div className='container mx-auto  w-full h-full pt-14 flex justify-center items-center'>
             <div className=' -mt-40 sm w-full max-w-6xl text-white flex flex-col gap-10 p-4'>
-              <h2 className='text-center sm:text-left text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-bold font-black'>My Portfolio</h2>
+              <h2 className='text-center sm:text-left text-4xl sm:text-5xl lg:text-6xl xl:text-7xl font-bold font-black font-bold font-black font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-[#80ff72] to-[#7ee8fa]'>PORTFOLIO</h2>
               <div className='h-1 md:h-1 w-full bg-green-400'></div>
               <h3 className='text-center md:text-left text-2xl sm:text-3xl lg:text-4xl xl:text-5xl '>Here are some examples of my work</h3>
             </div>
--- components/sections/resume/details.js
+++ components/sections/resume/details.js
@@ -4,20 +4,20 @@ import Image from 'next/image'
 const Details = () => {
   return (
     <section className='bg-slate-900 h-full w-full  text-white'>
-    <div className='container w-full h-full mx-auto flex p-4 sm:p-0 md:p-4 items-center max-w-6xl'>
-        <div className='w-full flex flex-col -mt-60 md:flex-row justify-between items-center mb-12'>
+    <div className=' w-full h-full mx-auto flex justify-center p-4 sm:p-0 md:p-4 items-center max-w-6xl'>
+        <div className=' container w-full flex flex-col -mt-64 md:flex-row justify-center items-center mb-12 gap-12'>
             <div className='w-[250px] h-[250px] sm:w-[400px] sm:h-[400px] md:w-[600px] md:h-[400px]lg:w-[700px] lg:h-[500px] relative'>
-                <Image src='/images/cv-img.svg' alt='Illustrtion of CV document' layout='fill' /> 
+                <Image src='/images/files.svg' alt='Illustrtion of CV document' layout='fill' /> 
             </div>
-            <div className='flex-col max-w-[420px]'>
+            <div className='flex-col max-w-[370px] lg:max-w-[420px]'>
                 <div className='font-bold  text-4xl mb-3 text-center sm:text-left'>A developer who was raised from sales and a lot of coffee.</div>
                 <div className='flex-grow text-center sm:text-left mb-4'>Front-end developer with a desire to write quality code and to create 
                 an amazing user experience. I{`'`}m looking for opportunities to grow and create something meaningful.
                 If you feel like you{`'`}re looking to hire someone like me, please don{`'`}t be hesitate. Grab a copy of
                 my resume and let{`'`}s talk.</div>
     
-                      <a href ="/cv.pdf" attributes-list download> 
-                <button className=' mx-auto sm:m-0 shadow-md bg-green-400 p-2 w-44 md:w-72 rounded-[4px] mt-4 hover:bg-green-300 flex justify-center items-center'>
+                      <a href ="/Tadas Delininkaitis CV.pdf" attributes-list download> 
+                <button className=' mx-auto md:m-0 shadow-md bg-green-400 p-2 w-44 md:w-72 rounded-[4px] mt-4 hover:bg-green-300 flex justify-center items-center'>
                     <p>Download Resume</p>
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
