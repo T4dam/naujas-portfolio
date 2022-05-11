03677dd0369328dfbb8f70a9de99c2e889279df0
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-11 15:37:39 +0300

    h and w

--- components/navbar/screendrop.js
+++ components/navbar/screendrop.js
@@ -7,7 +7,7 @@ const ScreenDrop = ({open, setOpen}) => {
       <>
       { open ?
 
-<div className='h-[100%] w-[100%] fixed z-[1] top-0 left-0 bg-slate-900/90 overflow-y-hidden transition duration-700 overscroll-none'>
+<div className='h-screen w-screen fixed z-[1] top-0 left-0 bg-slate-900/90 overflow-y-hidden transition duration-700 overscroll-none'>
           <button  onClick={()=>setOpen(prev=>!prev)} className='absolute top-10 right-10'>
           <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
           <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
