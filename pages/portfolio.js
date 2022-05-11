d58ba15ad68d321e5cecf352e18d57f25ac2a748
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-11 21:52:31 +0300

    Minor changes

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '../components/layouts';
 import PorfolioHero from '../components/sections/portfolio/portfolio-hero';
 import Mywork from '../components/sections/portfolio/my-work';
-import Head from 'next/head';
 
 const Portfolio = () => {
   return (
