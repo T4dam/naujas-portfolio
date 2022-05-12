76028a5295965ab97582ff776063ddb434b09372
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-12 22:10:36 +0300

    resume bug fix

--- components/sections/resume/details.js
+++ components/sections/resume/details.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Details = () => {
   return (
-    <section className='bg-slate-900 h-[600px] w-full  text-white'>
+    <section className='bg-slate-900 h-full w-full  text-white'>
     <div className=' w-full h-full mx-auto flex justify-center p-4 sm:p-0 md:p-4 items-center max-w-[640px] sm:max-w-[640px] md:max-w-[768px] lg:max-w-[1024px] xl:lg:max-w-[1152px] '>
         <div className=' container w-full flex flex-col  md:flex-row justify-center items-center mb-12 gap-12'>
             <div className='md:basis-full w-[250px] h-[250px] sm:w-[400px] sm:h-[400px] md:w-[600px] md:h-[400px]lg:w-[700px] lg:h-[500px] relative'>
