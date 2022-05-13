17fd6f026c64c8a5075667ce2b37af225284526d
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-13 08:15:42 +0300

    resume button fix

--- components/button/main-button.js
+++ components/button/main-button.js
@@ -6,6 +6,7 @@ const MainButton = (props) => {
     <Link href={props.link} passHref>
     <button className='shadow-md bg-green-400 py-2 px-5 sm:px-10 uppercase font-medium rounded-[4px] hover:bg-green-400 text-black transform hover:scale-110 transition ease-out duration-300'>
         {props.text}
+        {props.children}
     </button>
   
     </Link>
--- components/sections/resume/details.js
+++ components/sections/resume/details.js
@@ -17,9 +17,9 @@ const Details = () => {
                   <br />If you feel like you{`'`}re looking to hire someone like me, please don{`'`}t hesitate. Grab a copy of
                   my resume and let{`'`}s talk.</div>
                         <a href ="/Tadas_Delininkaitis_CV.pdf" attributes-list download>
-                  <button className=' mx-auto md:m-0 shadow-md bg-green-400 p-2 w-44 md:w-72 rounded-[4px] mt-4 hover:bg-green-300 flex justify-center items-center text-slate-900'>
+                  <button className=' flex items-center shadow-md bg-green-400 py-2 px-5 sm:px-10 uppercase font-medium rounded-[4px] hover:bg-green-400 text-black transform hover:scale-110 transition ease-out duration-300'>
                       <p>Download Resume</p>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
               </svg>
                       </button>
