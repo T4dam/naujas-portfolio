ccbe5169b192e8e8fe7beb2207ffefc253b93f51
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-05-18 14:39:58 +0300

    more animations

--- components/sections/cover/cover-hero.js
+++ components/sections/cover/cover-hero.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const CoverHero = () => {
   return (
     <section className='bg-slate-900 h-full w-full text-white'>
         <div className='container flex-col w-full h-full mx-auto flex p-4 sm:p-0 md:p-4 items-center'>
-        <div className='mt-6 self-start'>
+        <motion.div
+        whileHover={{
+          position:'relative',
+          zIndex: 1,
+          scale: [1, 1.4, 1.2],
+          rotate: [0, 10, -10, 0],
+          transition: {
+            duration: .2
+          }
+        }} className='mt-6 self-start'>
                 <Link href={'/portfolio'}>
                     <button className='flex'>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                       <path strokeLinecap="round" strokeLinejoin="round" d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z" />
                     </svg>Go back</button>
                 </Link>
-            </div>
+            </motion.div>
             <div className='bg-green-300 h-[500px] w-full max-w-5xl mx-auto  mt-14 mb-24 relative rounded-xl overflow-hidden'>
                 <Image src='/images/dogma.jpeg' alt='Cover art of Dogma group members' layout='fill' objectFit='cover' loading='eager' />
             </div>
