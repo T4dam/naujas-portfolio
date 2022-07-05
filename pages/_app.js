3c8ac6810422a276f89e270428514715e2eb621a
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-07-05 13:36:47 +0300

    google tag manager scripts

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import '../styles/globals.css'
+import React, { useState, useEffect } from 'react';
+import '../styles/globals.css';
 import Head from 'next/head';
-import { AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion';
 import Loader from '../components/loader';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(false);
+	const [loading, setLoading] = useState(false);
 
- useEffect(() =>{
-    setTimeout(() => setLoading(true), 2300);
-   })
+	useEffect(() => {
+		setTimeout(() => setLoading(true), 2300);
+	});
 
-  return (
-    <>
-      <Head>
-        <title>Tadel.lt Front-end Developer</title>
-        <meta name="description" content="Tadas' portfolio" />
-        <meta name="theme-color" content="#9333ea" />
-        <link rel="icon" href="/images/logo-purple.png" />
-      </Head>
-      {loading
-      ? (<AnimatePresence
-                exitBeforeEnter
-                initial={true}
-                onExitComplete={() => window.scrollTo(0, 0)}
-            >
-              <Component {...pageProps} />
-            </AnimatePresence>)
-
-      : (<Loader />)}
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<Script id="google-tag-manager" strategy="afterInteractive">
+					{`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N2MZZMB');
+`}
+				</Script>
+				<title>Tadel.lt Front-end Developer</title>
+				<meta name="description" content="Tadas' portfolio" />
+				<meta name="theme-color" content="#9333ea" />
+				<link rel="icon" href="/images/logo-purple.png" />
+			</Head>
+			{loading ? (
+				<AnimatePresence
+					exitBeforeEnter
+					initial={true}
+					onExitComplete={() => window.scrollTo(0, 0)}
+				>
+					{/* <!-- Google Tag Manager (noscript) --> */}
+					<noscript>
+						<iframe
+							src="https://www.googletagmanager.com/ns.html?id=GTM-N2MZZMB"
+							height="0"
+							width="0"
+							style="display:none;visibility:hidden"
+						></iframe>
+					</noscript>
+					{/* <!-- End Google Tag Manager (noscript) --> */}
+					<Component {...pageProps} />
+				</AnimatePresence>
+			) : (
+				<Loader />
+			)}
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
