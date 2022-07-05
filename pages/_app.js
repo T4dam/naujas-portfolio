b7130ad9a7d9b25f34c6ffb028ea38c8fa014977
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-07-05 13:47:39 +0300

    gtm add

--- pages/_app.js
+++ pages/_app.js
@@ -15,15 +15,16 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<Script id="google-tag-manager" strategy="afterInteractive">
-					{`
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+				<Script
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N2MZZMB');
-`}
-				</Script>
+})(window,document,'script','dataLayer','GTM-N2MZZMB');`,
+					}}
+				></Script>
 				<title>Tadel.lt Front-end Developer</title>
 				<meta name="description" content="Tadas' portfolio" />
 				<meta name="theme-color" content="#9333ea" />
