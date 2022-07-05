6f0b613a523a4b56bbe1faa81bbaf613df52ed46
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-07-05 15:09:29 +0300

    gtm npm

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import { AnimatePresence } from 'framer-motion';
 import Loader from '../components/loader';
-import Script from 'next/script';
+import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }) {
 	const [loading, setLoading] = useState(false);
@@ -15,16 +15,6 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
-				<Script
-					strategy="afterInteractive"
-					dangerouslySetInnerHTML={{
-						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N2MZZMB');`,
-					}}
-				></Script>
 				<title>Tadel.lt Front-end Developer</title>
 				<meta name="description" content="Tadas' portfolio" />
 				<meta name="theme-color" content="#9333ea" />
@@ -36,6 +26,9 @@ function MyApp({ Component, pageProps }) {
 					initial={true}
 					onExitComplete={() => window.scrollTo(0, 0)}
 				>
+					{useEffect(() => {
+						TagManager.initialize({ gtmId: 'GTM-N2MZZMB' });
+					}, [])}
 					<Component {...pageProps} />
 				</AnimatePresence>
 			) : (
