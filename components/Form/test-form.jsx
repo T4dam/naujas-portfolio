f5c84a673bc0d83178d466813f6a34c1febf0192
TADAS DELININKAITIS <t4dam94@gmail.com>
2022-07-27 13:00:52 +0300

    add sttring

--- components/Form/test-form.jsx
+++ components/Form/test-form.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const TestForm = () => {
 	return (
-		<form action="https://formsubmit.co/t4dam94@gmail.com" method="POST">
+		<form
+			action="https://formsubmit.co/8a35cbbd78c7c0d799a9abb22ad65087"
+			method="POST"
+		>
 			<input type="text" placeholder="message" name="message" required />
 			<input type="email" placeholder="email" name="email" required />
 			<input type="hidden" name="_captcha" value="false"></input>
